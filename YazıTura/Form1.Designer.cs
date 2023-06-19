9b54ed4e055802e5e24eb73b5a7edee089a859b3
insidecode-dev <trqtmhdv@gmail.com>
2023-06-19 07:50:49 +0400

    commit for yaziTura

---
+++ YazıTura/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace YazıTura
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1_guess = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1_guess
+            // 
+            this.button1_guess.Location = new System.Drawing.Point(253, 156);
+            this.button1_guess.Name = "button1_guess";
+            this.button1_guess.Size = new System.Drawing.Size(271, 86);
+            this.button1_guess.TabIndex = 0;
+            this.button1_guess.Text = "guess";
+            this.button1_guess.UseVisualStyleBackColor = true;
+            this.button1_guess.Click += new System.EventHandler(this.button1_guess_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1_guess);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1_guess;
+    }
+}
+
---
+++ YazıTura/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YazıTura
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Yazı Tura oyunu";
+        }
+        public string[] questions = { "Yes", "No" };
+        public string question = "";
+        Random rand = new Random();
+        DialogResult dialog;
+        private void button1_guess_Click(object sender, EventArgs e)
+        {
+            do
+            {
+                question = questions[rand.Next(0, 2)];
+                dialog = MessageBox.Show("Yazı mı ? Tura mı ? yazı=yes | tura=no", "Question", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Asterisk);
+            } while (dialog.ToString() != question);
+            MessageBox.Show("You win !", "Congrats");
+        }
+
+
+    }
+}
